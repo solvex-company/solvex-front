9cb39bde5098d4659bb5ead7053c5636e3537607
Mariana Lopez FSFT-61 <2013mariana.098@gmail.com>
2025-07-08 11:48:30 -0500

    Cambio de config en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'co.nixblix.com',
+        /* pathname: '/cdn/shop/files/**', */
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        /* pathname: '/cdn/shop/files/**', */
+      },
+    ],
+  },
 };
 
 export default nextConfig;

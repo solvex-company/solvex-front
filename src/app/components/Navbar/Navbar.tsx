85ec24a214f65d81830c57747d7bd779908e76c9
Mariana Lopez FSFT-61 <2013mariana.098@gmail.com>
2025-07-08 17:02:22 -0500

    Estructura de Carpetas

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Soy el Navbar</div>
+  )
+}
+
+export default Navbar
--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-  return (
-    // Esto es una prueba
-    <></>
-  );
-}

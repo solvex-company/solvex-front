fc5cc8cbe6c711d8480daf37550162676101b058
Mariana Lopez FSFT-61 <2013mariana.098@gmail.com>
2025-07-10 10:40:26 -0500

    Conexion front y back

---
+++ src/services/auth.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import axios from 'axios';
+import { FormikValues } from 'formik';
+
+const axiosApiBack = axios.create({
+    baseURL: process.env.API_URL,//localhost:4000
+});
+
+export const postRegister = async (data: FormikValues) => {
+  console.log(data);
+    try {
+       const res = await axiosApiBack.post('/auth/signup', data);
+
+       if (!res.data) {
+            return{
+                message: 'Error al registrar al usuario',
+                errors: res.data,
+            };
+       }
+       return {
+        message: 'Usuario registrado correctamente',
+       };
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.warn('Error al hacer el post de registro', error.message); // Error es seguro aquí
+        } 
+        return{
+            message: 'Error al registrar al usuario',
+            errors: (error as Error).message || 'Error desconocido',
+        };
+    }
+}

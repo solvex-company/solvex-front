8ea25780ed47840b5decfe785e118a962fd152bb
Franki09 <fgomezrosell@gmail.com>
2025-07-11 18:26:03 -0300

    Se agrego una seccion de planes en la home. Junto a correciion de otras cosas minimas.

--- src/app/(solvex)/(auth)/admin/dashboard/page.tsx
+++ src/app/(solvex)/(auth)/admin/dashboard/page.tsx
@@ -5,14 +5,14 @@ import AdminLicense from "./components/AdminLicense";
 
 function AdminDash() {
   return (
-    <div className="">
-      <h2 className="text-3xl font-bold pt-5">Datos del Usuario</h2>
+    <div>
+      <h2 className="text-3xl font-bold pt-5 underline">Datos del Usuario</h2>
       <AdminInfo />
 
       <h3 className="text-2xl font-bold pt-5">Cambiar contraseña</h3>
       <AdminPassword />
 
-      <h2 className="text-3xl font-bold pt-5">Tu Licencia / Suscripcion</h2>
+      <h2 className="text-2xl font-bold pt-5">Tu plan de Solvex</h2>
       <AdminLicense />
     </div>
   );

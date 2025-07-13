f16ae1cb3339650f97c163ddc016ffa5439c1e84
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-07-13 22:28:09 +0200

    Avance con el desarrollo de las cards y sume los labels a la pagina de login

--- src/app/(solvex)/(noAuth)/about/components/helper/developers.js
+++ src/app/(solvex)/(noAuth)/about/components/helper/developers.js
@@ -1,7 +1,7 @@
 const frontend = [
   {
     imgSrc:
-      "https://ik.imagekit.io/SolvexCompany/user.png?updatedAt=1752251986928",
+      "https://ik.imagekit.io/SolvexCompany/FrancoPaganoni.png?updatedAt=1752436422340",
     name: "Franco Paganoni",
     hobbies:
       "Desarrollador Full Stack, amante de la tecnología y la programación, siempre explorando nuevas herramientas y lenguajes para crear soluciones innovadoras.",
@@ -10,7 +10,7 @@ const frontend = [
   },
   {
     imgSrc:
-      "https://ik.imagekit.io/SolvexCompany/user.png?updatedAt=1752251986928",
+      "https://ik.imagekit.io/SolvexCompany/FrancoGomez.jpg?updatedAt=1752436421231",
     name: "Franco Gomez",
     hobbies:
       "Desarrollador Full Stack, amante de la tecnología y la programación, siempre explorando nuevas herramientas y lenguajes para crear soluciones innovadoras.",
@@ -19,7 +19,7 @@ const frontend = [
   },
   {
     imgSrc:
-      "https://ik.imagekit.io/SolvexCompany/user.png?updatedAt=1752251986928",
+      "https://ik.imagekit.io/SolvexCompany/MarianaLopez.jpg?updatedAt=1752436421313",
     name: "Mariana Lopez",
     hobbies:
       "Desarrollador Full Stack, amante de la tecnología y la programación, siempre explorando nuevas herramientas y lenguajes para crear soluciones innovadoras.",
@@ -49,7 +49,7 @@ const backend = [
   },
   {
     imgSrc:
-      "https://ik.imagekit.io/SolvexCompany/user.png?updatedAt=1752251986928",
+      "https://ik.imagekit.io/SolvexCompany/TobiasGonzalez.jpg?updatedAt=1752436421306",
     name: "Tobias Gonzalez",
     hobbies:
       "Desarrollador Full Stack, amante de la tecnología y la programación, siempre explorando nuevas herramientas y lenguajes para crear soluciones innovadoras.",
@@ -58,7 +58,7 @@ const backend = [
   },
   {
     imgSrc:
-      "https://ik.imagekit.io/SolvexCompany/user.png?updatedAt=1752251986928",
+      "https://ik.imagekit.io/SolvexCompany/AndreaLopez.jpg?updatedAt=1752436421303",
     name: "Andrea Lopez",
     hobbies:
       "Desarrollador Full Stack, amante de la tecnología y la programación, siempre explorando nuevas herramientas y lenguajes para crear soluciones innovadoras.",

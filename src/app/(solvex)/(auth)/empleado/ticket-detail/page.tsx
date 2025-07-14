a9af7429a08769e4d19e8966d6f6794b22e5730b
Franki09 <fgomezrosell@gmail.com>
2025-07-14 19:36:44 -0300

    Paginas de detalles de tickets terminadas.

--- src/app/(solvex)/(auth)/empleado/ticket-detail/page.tsx
+++ src/app/(solvex)/(auth)/empleado/ticket-detail/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import TicketDetail from "../../components/TicketDetail/TicketDetail";
+import { otherTicketData } from "@/helpers/ticketData";
 
 function EmTicketDetail() {
-  return <div>EmTicketDetail</div>;
+  return (
+    <div className="py-5 w-[967px]">
+      <h2 className="font-bold text-2xl pb-5">Detalle de MI ticket</h2>
+      <TicketDetail />
+      <div className="w-full pt-6">
+        <h3>Fecha</h3>
+        <p className="border border-accent rounded-md  p-2">{otherTicketData.date}</p>
+      </div>
+    </div>
+  );
 }
 
 export default EmTicketDetail;

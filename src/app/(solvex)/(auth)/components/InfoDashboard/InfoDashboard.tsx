d3147ac143a244121273b0abb38359bd5b78e9b7
MCLopezMoreno <2013mariana.098@gmail.com>
2025-07-18 12:08:26 -0500

    Google Auth completo

--- src/app/(solvex)/(auth)/components/InfoDashboard/InfoDashboard.tsx
+++ src/app/(solvex)/(auth)/components/InfoDashboard/InfoDashboard.tsx
@@ -5,7 +5,7 @@ import { useAuthContext } from "@/context/authContext";
 
 function InfoDashboard() {
   const { user } = useAuthContext();
-  const fullName = user ? `${user.name} ${user.lastname}` : `Nombre del usuario`;
+  const fullName = user?.name ? `${user.name} ${user.lastname}` : `Nombre del usuario`;
   return (
     <section className="grid grid-cols-2 justify-center items-center w-full gap-3 py-6">
       <div>
@@ -17,19 +17,19 @@ function InfoDashboard() {
       <div>
         <h2>Correo</h2>
         <h3 className="border border-accent rounded-lg p-1">
-          {user ? user.email : `Correo del usuario`}
+          {user ? user.email: `Correo del usuario`}
         </h3>
       </div>
       <div>
         <h2>Numero de identificacion (DNI)</h2>
         <h3 className="border border-accent rounded-lg p-1">
-          {user ? user.identification_number : `Numero de identificacion del usuario`}
+          {user?.identification_number ? user.identification_number : `Numero de identificacion del usuario`}
         </h3>
       </div>
       <div>
         <h2>Numero de Telefono</h2>
         <h3 className="border border-accent rounded-lg p-1">
-          {user ? user.phone : `Numero de telefono del usuario`}
+          {user?.phone ? user.phone : `Numero de telefono del usuario`}
         </h3>
       </div>
     </section>

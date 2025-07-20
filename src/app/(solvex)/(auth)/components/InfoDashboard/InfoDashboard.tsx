7ca0560935d76a7aed5107eea3155b72bf3c54ff
MCLopezMoreno <2013mariana.098@gmail.com>
2025-07-20 16:03:28 -0500

    Consumo del endpoint users/me para obtener info de usuario

--- src/app/(solvex)/(auth)/components/InfoDashboard/InfoDashboard.tsx
+++ src/app/(solvex)/(auth)/components/InfoDashboard/InfoDashboard.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { adminData } from "@/helpers/usersData";
 import { useAuthContext } from "@/context/AuthContext";
 
 function InfoDashboard() {
@@ -19,7 +18,7 @@ function InfoDashboard() {
         </h3>
       </div>
       <div>
-        <h2>Numero de identificacion (DNI)</h2>
+        <h2>Numero de identificacion</h2>
         <h3 className="border border-accent rounded-lg p-1">
           {user?.identification_number ? user.identification_number : `Numero de identificacion del usuario`}
         </h3>

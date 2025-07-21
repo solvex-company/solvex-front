2795a7ea31da7956f3c962d780ea374082cd120b
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-07-21 19:20:12 +0200

    Optimice la vista del administrador, donde puede cambiar roles y se visualiza de forma instantanea

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -6,6 +6,9 @@ import { ReactNode, useState } from "react";
 export const Provider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
+  // Add this line where you create your query client
+  // window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );

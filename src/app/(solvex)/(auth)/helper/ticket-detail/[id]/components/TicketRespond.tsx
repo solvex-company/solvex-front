2be2bad02096e2471f1e0a454d5df4df0e358cea
Franki09 <fgomezrosell@gmail.com>
2025-07-23 13:50:23 -0300

    Maquetacion de la respuesta de ticket bien acomodada. Falta conectarla con el back y que aparezca luego de darle a gestionar ticket

--- src/app/(solvex)/(auth)/helper/ticket-detail/[id]/components/TicketRespond.tsx
+++ src/app/(solvex)/(auth)/helper/ticket-detail/[id]/components/TicketRespond.tsx
@@ -23,13 +23,13 @@ function TicketRespond() {
   };
 
   return (
-    <div className="mt-10 h-[528px] bg-mainBg border border-secondText rounded-md">
-      <h2>Respuesta del Ticket</h2>
+    <div className="mt-10 bg-mainBg border border-secondText rounded-xl">
+      <h2 className="font-bold text-start text-2xl p-3 ml-2">Respuesta del Ticket</h2>
       <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleOnSubmit}>
         {(formik) => (
           <form onSubmit={formik.handleSubmit}>
-            <section className="flex gap-3">
-              <div className="flex flex-col ">
+            <section className="flex justify-around pb-4">
+              <div className="flex flex-col items-start ml-2 ">
                 <label htmlFor="title">Titulo</label>
                 <input
                   type="text"
@@ -37,11 +37,11 @@ function TicketRespond() {
                   name="title"
                   value={formik.values.title}
                   onChange={formik.handleChange}
-                  className="border border-accent rounded-md p-2 bg-white"
+                  className="border border-accent rounded-md p-2 h-[45px] w-[700px] bg-white"
                 />
                 {formik.touched.title && formik.errors.title && <p className="text-red-500 text-lg">{formik.errors.title}</p>}
               </div>
-              <div className="flex flex-col ">
+              <div className="flex flex-col mr-4 ">
                 <label htmlFor="date">Fecha de Respuesta</label>
                 <input
                   type="date"
@@ -49,27 +49,27 @@ function TicketRespond() {
                   id="date"
                   value={formik.values.date}
                   disabled
-                  className="border border-accent rounded-md p-2 h-[45px] w-full bg-white cursor-not-allowed"
+                  className="border border-accent rounded-md p-2 h-[45px] w-[200px] bg-white cursor-not-allowed"
                 />
               </div>
             </section>
 
-            <div className="flex flex-col ">
+            <div className="flex flex-col  items-start ml-4 mb-6">
               <label htmlFor="description">Descripcion</label>
               <textarea
                 id="description"
                 name="description"
                 value={formik.values.description}
                 onChange={formik.handleChange}
-                className="border border-accent rounded-md p-2 bg-white"
+                className="border border-accent rounded-md p-2 bg-white w-[923px] h-[170px]"
               />
               {formik.touched.description && formik.errors.description && (
                 <p className="text-red-500 text-lg">{formik.errors.description}</p>
               )}
             </div>
 
-            <section className="flex gap-3">
-              <div className="flex flex-col ">
+            <section className="flex justify-center  gap-4 w-[920px] pb-4 ml-4">
+              <div className="flex flex-col w-full ">
                 <label htmlFor="helper">Soporte que respondio el ticket</label>
                 <input
                   type="text"
@@ -78,16 +78,16 @@ function TicketRespond() {
                   value={formik.values.helper}
                   onChange={formik.handleChange}
                   disabled
-                  className="border border-accent rounded-md p-2 text-black bg-white cursor-not-allowed"
+                  className="border border-accent rounded-md p-2  h-[45px] w-max-[453px] text-black bg-white cursor-not-allowed"
                 />
               </div>
-              <div className="flex flex-col">
+              <div className="flex flex-col w-full">
                 <label htmlFor="status">Estado del ticket</label>
                 <select
                   name="status"
                   value={formik.values.status}
                   onChange={formik.handleChange}
-                  className="border border-accent rounded-md p-2 bg-white"
+                  className="border border-accent rounded-md p-2 bg-white h-[45px] w-max-[453px]"
                 >
                   <option value="">Selecciona el estado del Ticket</option>
                   <option value="pending">Pendiente</option>
@@ -100,12 +100,9 @@ function TicketRespond() {
 
             <button
               type="submit"
-              disabled={formik.isSubmitting}
-              className={`h-12 text-white text-xl font-bold p-2 rounded
-             ${formik.isSubmitting ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}
-            `}
+              className=" p-2 ml-4 my-4 h-12 w-[920px] text-white text-xl font-bold  rounded bg-blue-500 hover:bg-blue-600"
             >
-              {formik.isSubmitting ? "Enviando respuesta..." : "Responder ticket"}
+              Responder ticket
             </button>
           </form>
         )}

23cbb8b3f8b7e46fde23cde3aac7dcb16382249d
Franki09 <fgomezrosell@gmail.com>
2025-07-25 19:04:49 -0300

    Se arreglaron los errores locales de build que impedian el Deploy

--- src/app/(solvex)/(noAuth)/layout.tsx
+++ src/app/(solvex)/(noAuth)/layout.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import Footer from '../../components/Footer/Footer';
-import Navbar from '../../components/Navbar/Navbar';
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 
 export interface ILayout {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const LayoutTienda: React.FC<ILayout> = ({children}) => {
-    return (
-        <>
-            <Navbar />
-            {children}
-            <Footer />
-        </>
-    )
+const LayoutTienda: React.FC<ILayout> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default LayoutTienda;

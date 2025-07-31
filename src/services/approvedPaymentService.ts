57502ff3696cfcbc1a7ca62397280bc32dea5c2e
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2025-07-31 03:34:26 +0200

    Cambios en el renderizado condicional de algunos componentes

---
+++ src/services/approvedPaymentService.ts
@@ -0,0 +1,31 @@
+import AxiosApi from "@/app/api/axiosInstance";
+import axios from "axios";
+
+export interface approvedPaymentPayload {
+  paymentApproved: boolean;
+  token: string;
+}
+
+export const approvedPaymentService =
+  async (): Promise<approvedPaymentPayload> => {
+    try {
+      const response = await AxiosApi.get("/payments/is-approved");
+
+      if (response.data) {
+        console.log("Error, no llego la data");
+        throw new Error("Error al fetchear la data");
+      }
+
+      return response.data;
+    } catch (error: unknown) {
+      console.log("error de tipo: ", error);
+
+      if (axios.isAxiosError(error)) {
+        const errorMessage = error.response?.data?.message || error.message;
+
+        throw new Error("Error de axios ", errorMessage);
+      }
+
+      throw new Error("Error desconocido");
+    }
+  };
